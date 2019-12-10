e81076f2569bf2a3c39b31da09009b39eafc15e0
Gustl22 <user.rebo@gmx.de>
2019-12-10 18:01:19 +0100

    AO assignment 07 Task2b

--- assignments/solutions/AO-Assignment07/OMMExpressApp/routes/images.js
+++ assignments/solutions/AO-Assignment07/OMMExpressApp/routes/images.js
@@ -4,8 +4,27 @@ var router = express.Router();
 let imagesDB = require('./imagedatabase');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(imagesDB);
+router.get('/', function (req, res, next) {
+    res.json({images: imagesDB});
+});
+
+router.get('/:category', function (req, res, next) {
+    let images = imagesDB.filter(o => o.categories.includes(req.params.category));
+
+    if (req.query.hasOwnProperty('location')) {
+        images = images.filter(o => o.location.includes(req.query.location));
+    }
+    res.json({images: images});
+});
+
+router.post('/:category', function (req, res, next) {
+    imagesDB.push({
+        categories: [req.params.category],
+        url: req.body.url,
+        location: req.body.location
+    });
+    res.send();
+
 });
 
 module.exports = router;
